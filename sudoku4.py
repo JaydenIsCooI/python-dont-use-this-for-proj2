9be8c707719a4ddc4f3b48720b4c99ca7f75a901
Jayden Sterba <jaydensterba@gmail.com>
2026-03-26 21:44:25 -0500

    add sudoku4 - q4

---
+++ sudoku4.py
@@ -0,0 +1,40 @@
+﻿import clingo
+from clingo.application import Application, clingo_main
+from sudoku_board import Sudoku
+
+SUDOKU_PROGRAM = """
+cell(1..9).
+num(1..9).
+grid(1..9).
+
+1 { sudoku(X,Y,N) : num(N) } 1 :- cell(X), cell(Y).
+
+:- sudoku(X,Y,N), sudoku(X,Y2,N), Y < Y2.
+:- sudoku(X,Y,N), sudoku(X2,Y,N), X < X2.
+
+subgrid(X,Y,G) :- cell(X), cell(Y), G = 1 + ((X-1)/3)*3 + (Y-1)/3.
+:- grid(G), num(N), 2 <= #count { X,Y : sudoku(X,Y,N), subgrid(X,Y,G) }.
+
+:- initial(X,Y,N), not sudoku(X,Y,N).
+
+#show sudoku/3.
+"""
+
+
+class ClingoApp(Application):
+    def print_model(self, model, printer):
+        sudoku = Sudoku.from_model(model)
+        print(str(sudoku))
+
+    def main(self, ctl, files):
+        ctl.add("base", [], SUDOKU_PROGRAM)
+
+        for file in files:
+            ctl.load(file)
+
+        ctl.ground([("base", [])])
+        ctl.solve()
+
+
+if __name__ == "__main__":
+    clingo_main(ClingoApp())
